124bdb337557d24ad079242fcfa357764af618b8
binhussen <mhdhsna@gmail.com>
2022-10-21 07:51:16 +0300

    Insert Data to owner and species

--- data.sql
+++ data.sql
@@ -12,3 +12,17 @@ VALUES
 ('Boarmon', '2005-06-07', 7, true, 20.4, NULL),
 ('Blossom', '1998-10-13', 3, true, 17, NULL),
 ('Ditto', '2022-05-14', 4, true, 22, NULL);
+
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
